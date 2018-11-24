d6786da8e1ec47b9afea777db4f36fb59a2632d3
Antti Pirskanen <antti.a.pirskanen@gmail.com>
2018-11-24 18:36:07 +0200

    matrix path find works on some cases

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $.get('https://raw.githubusercontent.com/anttipirskanen/sanajahtihack/master/kot
 
 function start(xmlData) {
     //var letters = prompt("Insert letters without spaces");
-    var letters = 'axxnaknexkoxxxxx';
+    var letters = 'xxxnakneakoxxxxx';
     console.log(letters, xmlData);
     if(letters == null) 
         return false;
@@ -28,6 +28,12 @@ function start(xmlData) {
                 matrix.push(row);
             }
         }
+        matrix = [
+            ['x', 'x', 'x', 'n'],
+            ['a', 'k', 'n', 'e'],
+            ['a', 'k', 'o', 'x'],
+            ['x', 'x', 'x', 'x'],
+        ];
         return matrix;
     }
 
@@ -40,7 +46,7 @@ function start(xmlData) {
             console.log(coordinates);
             if(coordinates) {
                 count ++;
-                $('#word-list').append('<li>'+word.join('')+'</li>');
+                $('#word-list').append(getMatrixTable(word, letterMatrix(), coordinates));
             }
         }
 
@@ -72,11 +78,10 @@ function findWordPath(word, letterIndex, letterMatrix, coords, usedCoords) {
             for(var x = 0; x < letterMatrix.length; x++) {
                 for(var y = 0; y < letterMatrix[x].length; y++) {
                     if(letterMatrix[x][y] == word[letterIndex]) {
+                        console.log( word[letterIndex], {x:x, y:y});
                         return findWordPath(word, letterIndex + 1, letterMatrix, {x:x, y:y}, [{x:x, y:y}]);
                     }
-                    else {
-                        return false; 
-                    }
+ 
                 }
             }
         }
@@ -108,17 +113,20 @@ function findWordPath(word, letterIndex, letterMatrix, coords, usedCoords) {
                 //if coordinates are already used, ignore them
                 if(!ignoreCoords) {
                     found = checkCharAt(word[letterIndex], testCoords[i].x, testCoords[i].y, letterMatrix);
-                } 
+                }
+                
+                if(found) {
+                    console.log(word[letterIndex], coords);
+                    coords = {x : testCoords[i].x, y: testCoords[i].y};
+                    usedCoords.push(coords);
+                }
 
                 //Entire word is found, return char coordinates
                 if(found && (letterIndex == word.length-1)) {
-                    console.log('FOUND', usedCoords);
                     return usedCoords;
                 }
                 //Char is found, lets see if we can find next one
                 else if(found) {
-                    coords = {x : testCoords[i].x, y: testCoords[i].y};
-                    usedCoords.push(coords);
                     return findWordPath(word, letterIndex+1, letterMatrix, coords, usedCoords);
                 }
             }
@@ -131,7 +139,7 @@ function findWordPath(word, letterIndex, letterMatrix, coords, usedCoords) {
     }
 }
 
-function checkCharAt(char, y, x, matrix) {
+function checkCharAt(char, x, y, matrix) {
     try { 
         if(matrix[x][y] == char) {
             return true;
@@ -144,12 +152,40 @@ function checkCharAt(char, y, x, matrix) {
     }
 }
 
-var letters = [
-    ['A', 'X', 'X', 'N'],
-    ['A', 'K', 'N', 'E'],
-    ['X', 'K', 'O', 'X'],
-    ['X', 'X', 'X', 'X'],
-];
+//Create html of matrix, and show order of characters somewhow
+function getMatrixTable(word, matrix, coordinates) {
+    
+    var orderMatrix = JSON.parse(JSON.stringify(matrix));
+    for(var x = 0; x < orderMatrix.length; x++) {
+        for(var y = 0; y < orderMatrix[x].length; y++) {
+            orderMatrix[x][y] = null;
+        }
+    }
+
+    for(var i = 0; i < coordinates.length; i++) {    
+        orderMatrix[coordinates[i].x][coordinates[i].y] = i;
+    }
+    var ret = '<table>';
+    ret += '<tr><th colspan="4">'+word.join('')+'</th></tr>';
+    var count = 0;
+    for(var x = 0; x < matrix.length; x++) {
+        ret += '<tr>';
+        for(var y = 0; y < matrix[x].length; y++) {   
+            
+            if(orderMatrix[x][y] != null) {
+                count = count +2;
+                ret += '<td style="color:red;">' + matrix[x][y]+ '</td>';
+            }
+            else {
+                ret += '<td>' + matrix[x][y]+ '</td>';
+            } 
+        }
+        ret += '</tr>'
+    }
+    ret += '</table>';
+    return ret;
+}
+
 
 
 
